949c8133cb27b800e358b25b91061b0248c9222e
David George <david@setr.co.uk>
2015-11-17 22:25:02 +0000

    made a unicode pipeline to make sure i get korean stuff through and raise a dropitem when a hit is blank

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -5,7 +5,23 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+import codecs
 
-class TutorialPipeline(object):
+from scrapy.exceptions import DropItem
+
+
+class UnicodePipeline(object):
+    def __init__(self):
+        self.file = codecs.open('items.json', 'w', encoding='utf-8')
+
     def process_item(self, item, spider):
-        return item
+        if len(item['text']) > 0:
+            line = json.dumps(dict(item), ensure_ascii=False) + '\n'
+            self.file.write(line)
+            return item
+        else:
+            raise DropItem('No text found')
+
+    def spider_closed(self, spider):
+        self.file.close()
